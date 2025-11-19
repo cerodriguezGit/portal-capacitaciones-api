988455c4780ce910bce0828de368749395daf073
Carlos Eduardo Rodriguez <carlos96_007@hotmail.com>
2025-11-19 16:55:38 -0500

    Initial commit

---
+++ src/main/java/com/cerg/portal/capacitacionesapi/CapacitacionesApiApplication.java
@@ -0,0 +1,13 @@
+package com.cerg.portal.capacitacionesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacitacionesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacitacionesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cerg/portal/capacitacionesapi/CapacitacionesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerg.portal.capacitacionesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacitacionesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
